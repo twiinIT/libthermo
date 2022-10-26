0f2ac86943b8d1e5a542c9722e38e30ef40d94eb
Adrien Delsalle <ad.delsalle@gmail.com>
2022-10-26 18:44:16 +0200

    prepare packaging (#10)
    update license files
    rename history to changelog, add changelog for pythermo
    add Python package structure for pythermo
    remove dangling files
    add version for both lib/py thermo
    rename bindings output pythermo_core
    move pythermo bindings files in src dir
    install locally

---
+++ include/libthermo/version.hpp
@@ -0,0 +1,24 @@
+// Copyright (c) 2021-2022, twiinIT
+//
+// Distributed under the terms of the BSD 3-Clause License.
+//
+// The full license is in the file LICENSE, distributed with this software.
+
+#ifndef LIBTHERMO_VERSION_HPP
+#define LIBTHERMO_VERSION_HPP
+
+#define LIBTHERMO_VERSION_MAJOR 0
+#define LIBTHERMO_VERSION_MINOR 1
+#define LIBTHERMO_VERSION_PATCH 0
+
+#define __LIBTHERMO_STRINGIZE_IMPL(s) #s
+#define __LIBTHERMO_STRINGIZE(s) __LIBTHERMO_STRINGIZE_IMPL(s)
+
+#define LIBTHERMO_VERSION                                                                          \
+    (LIBTHERMO_VERSION_MAJOR * 10000 + LIBTHERMO_VERSION_MINOR * 100 + LIBTHERMO_VERSION_PATCH)
+#define LIBTHERMO_VERSION_STRING                                                                   \
+    __LIBTHERMO_STRINGIZE(LIBTHERMO_VERSION_MAJOR)                                                 \
+    "." __LIBTHERMO_STRINGIZE(LIBTHERMO_VERSION_MINOR) "." __LIBTHERMO_STRINGIZE(                  \
+        LIBTHERMO_VERSION_PATCH)
+
+#endif
---
+++ pythermo/pythermo/__init__.py
@@ -0,0 +1,4 @@
+try:
+    from pythermo.pythermo_core import *  # noqa: F401,F403
+except ImportError:
+    pass
