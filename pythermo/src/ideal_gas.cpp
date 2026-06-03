1ea226be67b70006e8c31cc7ea6f9cf506dcb15c
Adrien DELSALLE <ad.delsalle@gmail.com>
2026-06-03 14:04:11 +0200

    use dedicated methods without iterations instead of overloads

--- pythermo/src/ideal_gas.cpp
+++ pythermo/src/ideal_gas.cpp
@@ -13,46 +13,51 @@ using namespace thermo;
 
 namespace pythermo
 {
-    void ideal_gas(nb::module_& m)
+    class PyIdealGas : public PyThermo<IdealGas<double>>
+    {
+    public:
+        PyIdealGas(double r, double cp)
+        {
+            p_gas = std::make_unique<IdealGas<double>>(r, cp);
+        }
+
+        double static_t_no_iter(const double& tt, const double& mach) const
+        {
+            return p_gas->static_t(tt, mach);
+        }
+
+        double t_f_h_no_iter(const double& h) const
+        {
+            return p_gas->t_f_h(h);
+        }
+
+        double t_f_phi_no_iter(const double& phi) const
+        {
+            return p_gas->t_f_phi(phi);
+        }
+
+        double t_f_pr_no_iter(const double& pr, const double& t1, const double& eff_poly) const
+        {
+            return p_gas->t_f_pr(pr, t1, eff_poly);
+        }
+    };
+
+    void bind_ideal_gas(nb::module_& m)
     {
         using namespace nb::literals;
 
-        auto gas = nb::class_<IdealGas<double>>(m, "IdealGas");
-        gas.def(nb::init<double, double>());
+        auto gas = nb::class_<PyIdealGas>(m, "IdealGas").def(nb::init<double, double>());
 
-        bind_thermo_extended_interface<IdealGas<double>>(gas);
+        bind_thermo_extended_interface<PyIdealGas>(gas);
 
-        // ideal_gas.def(nb::pickle(
-        //     [](const IdealGas<double>& g) -> nb::tuple {  // __getstate__
-        //         return nb::make_tuple(g.r(), g.cp(288.15));
-        //     },
-        //     [](nb::tuple t) -> IdealGas<double> {  // __setstate__
-        //         IdealGas<double> g(t[0].cast<double>(), t[1].cast<double>());
-        //         return g;
-        //     }));
+        gas.def("__getstate__", [](const PyIdealGas& g) -> nb::tuple { return nb::make_tuple(g.r(), g.cp(288.15)); })
+            .def("__setstate__",
+                 [](PyIdealGas& g, const nb::tuple& state) -> void
+                 { new (&g) PyIdealGas(nb::cast<double>(state[0]), nb::cast<double>(state[1])); });
 
-        gas.def("static_t",
-                nanobind::overload_cast<const double&, const double&>(&IdealGas<double>::static_t, nanobind::const_),
-                "Static temperature",
-                "tt"_a,
-                "mach"_a);
-
-        gas.def("t_f_h",
-                nanobind::overload_cast<const double&>(&IdealGas<double>::t_f_h, nanobind::const_),
-                "Temperature from enthalpy",
-                "h"_a);
-
-        gas.def("t_f_phi",
-                nanobind::overload_cast<const double&>(&IdealGas<double>::t_f_phi, nanobind::const_),
-                "Temperature from phi function",
-                "h"_a);
-
-        gas.def("t_f_pr",
-                nanobind::overload_cast<const double&, const double&, const double&>(&IdealGas<double>::t_f_pr,
-                                                                                     nanobind::const_),
-                "Temperature from pressure ratio",
-                "pr"_a,
-                "t1"_a,
-                "eff_poly"_a);
+        gas.def("static_t", &PyIdealGas::static_t_no_iter, "Static temperature", "tt"_a, "mach"_a);
+        gas.def("t_f_h", &PyIdealGas::t_f_h_no_iter, "Temperature from enthalpy", "h"_a);
+        gas.def("t_f_phi", &PyIdealGas::t_f_phi_no_iter, "Temperature from phi function", "h"_a);
+        gas.def("t_f_pr", &PyIdealGas::t_f_pr_no_iter, "Temperature from pressure ratio", "pr"_a, "t1"_a, "eff_poly"_a);
     }
 }
