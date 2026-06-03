a8ac7dbbd2a37e302250f4b1c7d5546fc36bbc0c
Adrien DELSALLE <ad.delsalle@gmail.com>
2026-06-03 14:00:12 +0200

    use back templated props type

--- include/libthermo/ideal_gas.hpp
+++ include/libthermo/ideal_gas.hpp
@@ -17,11 +17,11 @@
 
 namespace thermo
 {
-    template <class T>
-    class IdealGas : public Thermo<IdealGas<T>>
+    template <class PropsType>
+    class IdealGas : public Thermo<IdealGas<PropsType>>
     {
     public:
-        IdealGas(const T& r, const T& cp)
+        IdealGas(const PropsType& r, const PropsType& cp)
             : m_r(r)
             , m_cp(cp)
             , m_gamma(cp / (cp - r)) {};
@@ -32,141 +32,173 @@ namespace thermo
             , m_cp(props.cp())
             , m_gamma(props.cp() / (props.cp() - props.r())) {};
 
+        template <class T>
         auto gamma(const T& t) const;
 
+        template <class T>
         auto cp(const T& = 0.) const;
 
+        template <class T>
         auto phi(const T& t) const;
 
+        template <class T>
         auto pr(const T& t1, const T& t2, const T& eff_poly) const;
 
+        template <class T>
         auto Tau(const T& p1, const T& p2, const T& eff_poly) const;
 
+        template <class T>
         auto eff_poly(const T& p1, const T& t1, const T& p2, const T& t2) const;
 
+        template <class T>
         auto h(const T& t) const;
 
         auto r() const;
 
+        template <class T>
         auto static_t(const T& tt, const T& mach) const;
+        template <class T>
         auto static_t(const T& tt, const T& mach, double, std::size_t = 1) const;
 
+        template <class T>
         auto t_f_pr(const T& pr, const T& t1, const T& eff_poly) const;
+        template <class T>
         auto t_f_pr(const T& pr, const T& t1, const T& eff_poly, double, std::size_t = 1) const;
 
+        template <class T>
         auto t_f_h(const T& h) const;
+        template <class T>
         auto t_f_h(const T& h, double, std::size_t = 1) const;
 
+        template <class T>
         auto t_f_phi(const T& h) const;
+        template <class T>
         auto t_f_phi(const T& h, double, std::size_t = 1) const;
 
+        template <class T>
         auto mach_f_wqa(const T& pt, const T& tt, const T& wqa, double, std::size_t = 1) const;
 
     protected:
-        T m_r, m_cp, m_gamma;
+        PropsType m_r, m_cp, m_gamma;
 
         template <class S>
         friend bool operator==(const IdealGas<S>& ref, const IdealGas<S>& other);
     };
 
+    template <class PropsType>
     template <class T>
-    auto IdealGas<T>::gamma(const T&) const
+    auto IdealGas<PropsType>::gamma(const T&) const
     {
         return m_gamma;
     }
 
+    template <class PropsType>
     template <class T>
-    auto IdealGas<T>::cp(const T&) const
+    auto IdealGas<PropsType>::cp(const T&) const
     {
         return m_cp;
     }
 
+    template <class PropsType>
     template <class T>
-    auto IdealGas<T>::phi(const T& t) const
+    auto IdealGas<PropsType>::phi(const T& t) const
     {
         return m_cp * std::log(t);
     }
 
+    template <class PropsType>
     template <class T>
-    auto IdealGas<T>::pr(const T& t1, const T& t2, const T& eff_poly) const
+    auto IdealGas<PropsType>::pr(const T& t1, const T& t2, const T& eff_poly) const
     {
         return std::exp(std::log(t2 / t1) * eff_poly * m_cp / m_r);
     }
 
+    template <class PropsType>
     template <class T>
-    auto IdealGas<T>::Tau(const T& p1, const T& p2, const T& eff_poly) const
+    auto IdealGas<PropsType>::Tau(const T& p1, const T& p2, const T& eff_poly) const
     {
         return std::exp(std::log(p2 / p1) * m_r / (eff_poly * m_cp));
     }
 
+    template <class PropsType>
     template <class T>
-    auto IdealGas<T>::eff_poly(const T& p1, const T& t1, const T& p2, const T& t2) const
+    auto IdealGas<PropsType>::eff_poly(const T& p1, const T& t1, const T& p2, const T& t2) const
     {
         return r() / m_cp * log(p2 / p1) / std::log(t2 / t1);
     }
 
+    template <class PropsType>
     template <class T>
-    auto IdealGas<T>::h(const T& t) const
+    auto IdealGas<PropsType>::h(const T& t) const
     {
         return m_cp * t;
     }
 
-    template <class T>
-    auto IdealGas<T>::r() const
+    template <class PropsType>
+    auto IdealGas<PropsType>::r() const
     {
         return m_r;
     }
 
+    template <class PropsType>
     template <class T>
-    auto IdealGas<T>::static_t(const T& tt, const T& mach) const
+    auto IdealGas<PropsType>::static_t(const T& tt, const T& mach) const
     {
         return tt / (1 + 0.5 * (m_cp / (m_cp - m_r) - 1.) * std::pow(mach, 2.));
     }
 
+    template <class PropsType>
     template <class T>
-    auto IdealGas<T>::static_t(const T& tt, const T& mach, double, std::size_t) const
+    auto IdealGas<PropsType>::static_t(const T& tt, const T& mach, double, std::size_t) const
     {
         return static_t(tt, mach);
     }
 
+    template <class PropsType>
     template <class T>
-    auto IdealGas<T>::t_f_pr(const T& pr, const T& t1, const T& eff_poly) const
+    auto IdealGas<PropsType>::t_f_pr(const T& pr, const T& t1, const T& eff_poly) const
     {
         return t1 * std::pow(pr, m_r / (m_cp * eff_poly));
     }
 
+    template <class PropsType>
     template <class T>
-    auto IdealGas<T>::t_f_pr(const T& pr, const T& t1, const T& eff_poly, double, std::size_t) const
+    auto IdealGas<PropsType>::t_f_pr(const T& pr, const T& t1, const T& eff_poly, double, std::size_t) const
     {
         return t_f_pr(pr, t1, eff_poly);
     }
 
+    template <class PropsType>
     template <class T>
-    auto IdealGas<T>::t_f_h(const T& h) const
+    auto IdealGas<PropsType>::t_f_h(const T& h) const
     {
         return h / m_cp;
     }
 
+    template <class PropsType>
     template <class T>
-    auto IdealGas<T>::t_f_h(const T& h, double, std::size_t) const
+    auto IdealGas<PropsType>::t_f_h(const T& h, double, std::size_t) const
     {
         return t_f_h(h);
     }
 
+    template <class PropsType>
     template <class T>
-    auto IdealGas<T>::t_f_phi(const T& phi) const
+    auto IdealGas<PropsType>::t_f_phi(const T& phi) const
     {
         return std::exp(phi / m_cp);
     }
 
+    template <class PropsType>
     template <class T>
-    auto IdealGas<T>::t_f_phi(const T& phi, double, std::size_t) const
+    auto IdealGas<PropsType>::t_f_phi(const T& phi, double, std::size_t) const
     {
         return t_f_phi(phi);
     }
 
+    template <class PropsType>
     template <class T>
-    auto IdealGas<T>::mach_f_wqa(const T& pt, const T& tt, const T& wqa, double tol, std::size_t max_iter) const
+    auto IdealGas<PropsType>::mach_f_wqa(const T& pt, const T& tt, const T& wqa, double tol, std::size_t max_iter) const
     {
         double a = wqa * sqrt(tt) / pt * sqrt(m_r / m_gamma);
         double coeff = 0.5 * (m_gamma + 1.) / (1. - m_gamma);
@@ -189,8 +221,8 @@ namespace thermo
         return res.first;
     }
 
-    template <class T>
-    inline bool operator==(const IdealGas<T>& ref, const IdealGas<T>& other)
+    template <class PropsType>
+    inline bool operator==(const IdealGas<PropsType>& ref, const IdealGas<PropsType>& other)
     {
         return ref.m_cp == other.m_cp && ref.m_r == other.m_r;
     }
